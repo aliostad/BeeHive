6d7e8f854f217292330397ec0fd61f97a39b20a9
Ali Kheyrollahi <aliostad@gmail.com>
2014-05-21 23:02:34 +0100

    setup

--- sample/BeeHive.Sample.NewsFeedKeywordNotification.Worker/WorkerRole.cs
+++ sample/BeeHive.Sample.NewsFeedKeywordNotification.Worker/WorkerRole.cs
@@ -3,7 +3,17 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Threading;
+using BeeHive.Actors;
+using BeeHive.Azure;
+using BeeHive.Configuration;
+using BeeHive.DataStructures;
+using BeeHive.Sample.NewsFeedKeywordNotification.Impl.Events;
+using BeeHive.Scheduling;
+using BeeHive.ServiceLocator.Windsor;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
 using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Diagnostics;
 using Microsoft.WindowsAzure.ServiceRuntime;
@@ -13,6 +23,27 @@ namespace BeeHive.Sample.NewsFeedKeywordNotification.Worker
 {
     public class WorkerRole : RoleEntryPoint
     {
+
+        private IConfigurationValueProvider _configurationValueProvider;
+        private List<IPulser> _pulsers;
+        private PulserPublisher _pulserPublisher;
+        private Orchestrator _orchestrator;
+
+        public WorkerRole()
+        {
+            _pulsers = Pulsers.FromAssembly(Assembly.GetExecutingAssembly())
+                .ToList();
+
+
+            var container = new WindsorContainer();
+
+            _configurationValueProvider = new AzureConfigurationValueProvider();
+            SetupDi(container, _configurationValueProvider, _pulsers.ToArray());
+            _pulserPublisher = container.Resolve<PulserPublisher>();
+            _orchestrator = container.Resolve<Orchestrator>();
+
+        }
+
         public override void Run()
         {
             // This is a sample worker implementation. Replace with your logic.
@@ -25,6 +56,77 @@ namespace BeeHive.Sample.NewsFeedKeywordNotification.Worker
             }
         }
 
+        public static void SetupDi(IWindsorContainer container,
+            IConfigurationValueProvider configurationValueProvider,
+            IPulser[] pulsers)
+                {
+            var serviceLocator = new WindsorServiceLocator(container);
+
+            container.Register(
+
+                Component.For<Orchestrator>()
+                .ImplementedBy<Orchestrator>()
+                .LifestyleTransient(),
+
+                Component.For<IActorConfiguration>()
+                    .Instance(
+                    ActorDescriptors.FromAssemblyContaining<NewsItemCaptured>()
+                    .ToConfiguration()),
+
+                Component.For<IServiceLocator>()
+                    .Instance(serviceLocator),
+
+                Component.For<IPulser[]>()
+                    .Instance(pulsers),
+
+                Component.For<PulserPublisher>()
+                .ImplementedBy<PulserPublisher>()
+                .LifestyleSingleton(),
+
+                Component.For<IFactoryActor>()
+                    .ImplementedBy<FactoryActor>()
+                    .LifestyleTransient(),
+
+                
+
+                Component.For<IEventQueueOperator>()
+                    .ImplementedBy<ServiceBusOperator>()
+                    .DynamicParameters((k, dic)
+                        =>
+                    {
+                        dic["connectionString"] = configurationValueProvider.GetValue(
+                            "BusConnectionString");
+                    })
+                    .LifestyleSingleton(),
+
+               Component.For(typeof(ICollectionStore<>))
+                .ImplementedBy(typeof(AzureCollectionStore<>))
+                 .DynamicParameters((k, dic)
+                        =>
+                 {
+                     dic["connectionString"] = configurationValueProvider.GetValue("StorageConnectionString");
+                 })
+                .LifestyleSingleton(),
+
+                Component.For(typeof(IKeyValueStore))
+                .ImplementedBy(typeof(AzureKeyValueStore))
+                 .DynamicParameters((k, dic)
+                        =>
+                 {
+                     dic["connectionString"] = configurationValueProvider.GetValue("StorageConnectionString");
+                     dic["bucketName"] = "feeds";
+                 })
+                .LifestyleSingleton(),
+               
+                Classes.FromAssemblyContaining<NewsFeedPulsed>()
+                    .Pick()
+                
+            );
+
+
+        }
+
+
         public override bool OnStart()
         {
             // Set the maximum number of concurrent connections 
@@ -33,7 +135,19 @@ namespace BeeHive.Sample.NewsFeedKeywordNotification.Worker
             // For information on handling configuration changes
             // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
 
+            _orchestrator.SetupAsync().Wait();
+            _orchestrator.Start();
+            _pulsers.ForEach(x => x.Start());
+
             return base.OnStart();
+        }
+
+
+        public override void OnStop()
+        {
+            _orchestrator.Stop();
+            _pulsers.ForEach(x => x.Stop());
+            base.OnStop();
         }
     }
 }
